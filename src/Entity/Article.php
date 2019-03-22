d8434f21345983725a48a41769b1492d6346ba14
Airat Akhmetshin <st1ncnpk@gmail.com>
2019-03-22 16:20:02 +0300

    add Article Entity

---
+++ src/Entity/Article.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SportsruApi\Entity;
+
+class Article
+{
+    /** @var bool */
+    private $advertMaterial;
+
+    /** @var bool */
+    private $bodyIsEmpty;
+
+    /** @var bool */
+    private $calcStat;
+
+    /** @var int */
+    private $commentsCount;
+
+    /** @var bool */
+    private $commentsDisabled;
+
+    /** @var string */
+    private $contentOrigin;
+
+    /** @var string */
+    private $desktopUrl;
+
+    /** @var int */
+    private $id;
+
+    /** @var string */
+    private $image;
+
+    
+//"main": false,
+//"main_in_section": true,
+//"media_type": "none",
+//"mobile_url": "https://m.sports.ru/boxing/1070791477.html",
+//"partner_material": false,
+//"published": {},
+//"section": {},
+//"social_image": {},
+//"source": {},
+//"special": false,
+//"special_without_feed": false,
+//"title": "У Грегора Гиллеспи украли сумку во время UFC на ESPN+",
+
+
+}
