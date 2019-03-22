1a747cb607419c82eb4929b0dccd77fd34c43abb
Airat Akhmetshin <st1ncnpk@gmail.com>
2019-03-22 20:57:35 +0300

    add article entity

---
+++ src/Entity/Article.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace SportsruApi\Entity;
+
+class Article
+{
+    /** @var bool */
+    private $advertMaterial;
+
+    /** @var string */
+    private $articleType;
+
+    /** @var bool */
+    private $calcStat;
+
+    /** @var int */
+    private $commentsCount;
+
+    /** @var bool */
+    private $commentsDisabled;
+
+    /** @var string */
+    private $desktopUrl;
+
+    /** @var string */
+    private $feature;
+
+    /** @var array */
+    private $feedImage;
+
+    /** @var int */
+    private $id;
+
+    /** @var array */
+    private $image;
+
+    /** @var array */
+    private $images;
+
+    /** @var string */
+    private $mobileUrl;
+
+    /** @var bool */
+    private $partnerMaterial;
+
+    /** @var array */
+    private $published;
+
+    /** @var array */
+    private $section;
+
+    /** @var bool */
+    private $special;
+
+    /** @var bool */
+    private $specialWithoutFeed;
+
+    /** @var string */
+    private $title;
+
+    /** @var bool */
+    private $ugcMaterial;
+}
