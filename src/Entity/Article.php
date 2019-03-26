2d25d3c3f65e836788be829735546ad7db95ebb7
Airat Akhmetshin <st1ncnpk@gmail.com>
2019-03-26 16:25:24 +0300

    Убрал неиспользуемые поля в Article

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,29 +4,14 @@ namespace SportsruApi\Entity;
 
 class Article
 {
-    /** @var bool */
-    private $advertMaterial;
-
-    /** @var string */
-    private $articleType;
-
-    /** @var bool */
-    private $calcStat;
-
     /** @var int */
     private $commentsCount;
-
-    /** @var bool */
-    private $commentsDisabled;
 
     /** @var string */
     private $desktopUrl;
 
     /** @var string */
     private $feature;
-
-    /** @var array */
-    private $feedImage;
 
     /** @var int */
     private $id;
@@ -40,20 +25,11 @@ class Article
     /** @var string */
     private $mobileUrl;
 
-    /** @var bool */
-    private $partnerMaterial;
-
     /** @var array */
     private $published;
 
     /** @var array */
     private $section;
-
-    /** @var bool */
-    private $special;
-
-    /** @var bool */
-    private $specialWithoutFeed;
 
     /** @var string */
     private $title;
